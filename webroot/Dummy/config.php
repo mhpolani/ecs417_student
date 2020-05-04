cd0bdaf3eb520d4f668d19464de871341dde35cc
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-04 17:17:09 +0500

    changed

--- webroot/Dummy/config.php
+++ webroot/Dummy/config.php
@@ -5,9 +5,9 @@ $dbuser = getenv("user");
 $dbpwd = getenv("password");
 $dbname = getenv("database");
 // Creates connection
-$conn = new MySQLi($dbhost, $dbuser, $dbpwd, $dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 // Checks connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed!!!: " . $conn->connect_error);
 } 
 ?>
