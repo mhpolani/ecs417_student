eeec78d7a1ea7a04c8634467263faed5fd57d352
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 02:25:37 +0500

    changed

--- webroot/Blog/assets/database/connect.php
+++ webroot/Blog/assets/database/connect.php
@@ -23,22 +23,5 @@ else
 }
 
 
-$fname = $_POST["Hamza"];
-$sname = $_POST["Polani"];
-$email = $_POST["mhpolanto@gmail.com"];
-$pass1 = $_POST["pass1"];
-
-$sql = "INSERT INTO USERS VALUES ('$fname',
-'$sname', '$email', '$pass1')";
-
-if ($conn->query($sql) === TRUE) 
-{
-    echo("HAHA");   
-} 
-else 
-{
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();  
 
  
--- webroot/Blog/insert.php
+++ webroot/Blog/insert.php
@@ -1,20 +1,19 @@
-
+<?php
 $fname = $_POST["Hamza"];
 $sname = $_POST["Polani"];
 $email = $_POST["mhpolanto@gmail.com"];
 $pass1 = $_POST["pass1"];
-  if ($_SERVER['REQUEST_METHOD'] == 'POST'){ 
-$sql = "INSERT INTO USERS (firstName, lastName, email, password) VALUES ('$fname',
+
+$sql = "INSERT INTO USERS VALUES ('$fname',
 '$sname', '$email', '$pass1')";
-  
-if ($conn->query($sql) === TRUE) {
-    echo "<h6>Registration Successful</h6>
-    <p><a href='exercise1.html'>Home</a></p>
-    ";
-    echo("HAHA");
-   
-} else {
+
+if ($conn->query($sql) === TRUE) 
+{
+    echo("HAHABhai");   
+} 
+else 
+{
     echo "Error: " . $sql . "<br>" . $conn->error;
-}  
 }
- 
+$conn->close();  
+?>
