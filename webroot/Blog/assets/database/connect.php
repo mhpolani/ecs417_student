7ed6fb8365fe9fba5107116266cd40ff9ee9522a
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 02:50:13 +0500

    changed

--- webroot/Blog/assets/database/connect.php
+++ webroot/Blog/assets/database/connect.php
@@ -27,7 +27,7 @@ $sname = $_POST["Polani"];
 $email = $_POST["mhpolanto@gmail.com"];
 $pass1 = $_POST["pass1"];
 
-$sql = "INSERT INTO DETAILS(firstName,lastName,email,password) VALUES ('$fname',
+$sql = "INSERT INTO DETAILS(firstName,lastName,email,pw) VALUES ('$fname',
 '$sname', '$email', '$pass1')";
 
 if ($conn->query($sql) === TRUE) 
