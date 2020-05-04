4eb808342222bd2020c2bc7602364d2f980ed7fb
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 03:06:07 +0500

    changed

--- webroot/Blog/assets/database/connect.php
+++ webroot/Blog/assets/database/connect.php
@@ -22,13 +22,13 @@ else
     echo("yesYESYES");
 }
 
-$fname = $_POST["Hamza"];
-$sname = $_POST["Polani"];
-$email = $_POST["mhpolanto@gmail.com"];
-$pass1 = $_POST["pass1"];
+// $fname = $_POST["Hamza"];
+// $sname = $_POST["Polani"];
+// $email = $_POST["mhpolanto@gmail.com"];
+// $pass1 = $_POST["pass1"];
 
-$sql = "INSERT INTO USERS VALUES ('$fname',
-'$sname', '$email', '$pass1')";
+$sql = "INSERT INTO USERS(firstName,lastName,email,pw) VALUES ('Hamza',
+'Hamza', 'mhpolanto@gmail.com', '123')";
 
 if ($conn->query($sql) === TRUE) 
 {
