146a67f7071fd20d4ef15a76eb2fbdff52cc68f3
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-04 18:43:52 +0500

    changed

--- webroot/Blog/assets/database/connect.php
+++ webroot/Blog/assets/database/connect.php
@@ -16,4 +16,7 @@ $conn = new mysqli($servername, $username,$password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+else{
+    echo("yesYESYES");
+}
 ?>
