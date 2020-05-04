2da91991fb58a9b8bfb311739f32fd759a86ce8b
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-04 18:39:14 +0500

    changed

--- webroot/Blog/assets/database/connect.php
+++ webroot/Blog/assets/database/connect.php
@@ -1,13 +1,19 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db_name = 'blog';
-
-$conn = new MySQLi($host,$user,$pass,$db_name);
-
-if($con-> connect_error)
-{
-    die('Database connection error" ' . $conn->connect_error);
+/*
+$servername = "localhost";
+$username = "root";
+$password = "password123"
+$db = "login_details";
+*/
+$servername = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$username = getenv("DATABASE_USER");
+$password = getenv("DATABASE_PASSWORD");
+$db = getenv("DATABASE_NAME");
+// Creates connection
+$conn = new mysqli($servername, $username,$password, $db);
+// Checks connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
