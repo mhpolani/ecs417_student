8298b609d7cbba6f3ca897357633ab165b7fcb96
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 06:39:01 +0500

    changed

--- webroot/Blog/assets/database/db.php
+++ webroot/Blog/assets/database/db.php
@@ -1,18 +1,87 @@
 <?php
 require('connect.php');
 
+function dd($value) //to be deleted
+{
+  echo "<pre>", print_r($value,true),"</pre>";
+  die();
 
-function selectAll($table)
+function selectAll($table, $conditions =[])   //
 {
     global $conn;
     $sql = "SELECT * FROM $table";
+    if (empty($conditions))
+    {
     $stmt = $conn->prepare($sql);
     $stmt-> execute();
     $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     return $records;
+    }
+    else
+    {
+        // returns records that match conditions
+        $i = 0;
+        foreach ($conditions as $key => $value)
+        {
+            if($i == 0)
+            {   
+            $sql = $sql . " WHERE $key = value";
+            }
+            else 
+            {
+                $sql = $sql . " AND $key = ?";
+            }
+            $i++;
+        }
+        dd($sql);
+        $stmt = $conn->prepare($sql);
+        $values = array_values($conditions);
+        $types = str_repeat('s',count($values));
+        $stmt-> bind_param($types,...$values);
+        $stmt-> execute();
+        $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
+    }
 }
 
-$users = selectAll('USERS');
-echo "<pre>", print_r($users),"</pre>";
+function selectOne($table, $conditions)   //
+{
+    global $conn;
+    $sql = "SELECT * FROM $table";
+           // returns records that match conditions
+        $i = 0;
+        foreach ($conditions as $key => $value)
+        {
+            if($i == 0)
+            {   
+            $sql = $sql . " WHERE $key = value";
+            }
+            else 
+            {
+                $sql = $sql . " AND $key = ?";
+            }
+            $i++;
+        }
+        // $sql = "SELECT + FROM USERS where admin = 0 AND USERNAME = "HAMZA" LIMIT 1"  finds only one record
+        $sql = $sql ." LIMIT 1";
+        dd($sql);
+
+        $stmt = $conn->prepare($sql);
+        $values = array_values($conditions);
+        $types = str_repeat('s',count($values));
+        $stmt-> bind_param($types,...$values);
+        $stmt-> execute();
+        $records = $stmt->get_result()->fetch_assoc();   //MYSQLI_ASSOC
+        return $records;
+}
+
+$conditions = [
+    'admin' => 1,
+    'username' => 'Hamza'
+];
+// $users = selectAll('USERS',$conditions);  selectAll paramater conditions is an array
+$users = selectOne('USERS', $conditions);
+dd($users);
+
 
 ?>
