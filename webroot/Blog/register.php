4b2659c2edcc52db736d0e363400144eb39a2e18
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 02:28:03 +0500

    changed

--- webroot/Blog/register.php
+++ webroot/Blog/register.php
@@ -1,5 +1,5 @@
 
-<?php include('users.php'); ?>
+<?php include('users'.php); ?>
 
 <!DOCTYPE html>
  <html lang = "en">
--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -3,8 +3,8 @@ include("db2.php");
 
 if (isset($_POST['register-btn']))
 {
-    // unset($_POST['register-btn']);
-    // unset($_POST['passwordConfirmation']);
-    // $_POST['ad'] 
+    unset($_POST['register-btn']);
+    unset($_POST['passwordConfirmation']);
+    $_POST['ad'] = 0;
     dd($_POST);
 }
