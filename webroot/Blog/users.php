90988450d4f4794cef9d1fd908a290698f030e3a
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 15:02:10 +0500

    changed

--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -4,7 +4,8 @@ if (isset($_POST['register-btn']))
 {
    unset($_POST['register-btn'], $_POST['passwordConfirmation']);
    $_POST['admin'] = 0;
-   echo "<pre>", print_r($value,true),"</pre>";
-   die();
+   var_dump($_POST);
+//    echo "<pre>", print_r($value,true),"</pre>";
+   
 }
 ?>
