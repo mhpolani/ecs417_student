e1593eafc997e9b4fd9656b043720613a1b4352e
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 03:13:13 +0500

    changed

--- webroot/Blog/register.php
+++ webroot/Blog/register.php
@@ -1,5 +1,13 @@
-<?php include ('db2.php'); ?>
-<?php include ('users.php') ?>
+<?php include ('db.php'); ?>
+<?php if (isset($_POST['register-btn']))
+{
+    unset($_POST['register-btn']);
+    unset($_POST['passwordConfirmation']);
+    $_POST['ad'] = 0;
+    $_POST['pw'] = password_hash($_POST['pw'], PASSWORD_DEFAULT);
+    dd($_POST);
+} 
+?>
 
 <!DOCTYPE html>
  <html lang = "en">
--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -1,5 +1,5 @@
 <?php
-include("db2.php");
+include("db.php");
 
 if (isset($_POST['register-btn']))
 {
