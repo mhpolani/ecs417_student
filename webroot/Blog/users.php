46d7dfa0783e0d48f8c06c26c555cae4e58bca8e
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 02:54:43 +0500

    changed

--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -7,5 +7,6 @@ if (isset($_POST['register-btn']))
     unset($_POST['passwordConfirmation']);
     $_POST['ad'] = 0;
     $_POST['pw'] = password_hash($_POST['pw'], PASSWORD_DEFAULT);
-    dd($_POST);
+    echo "<pre>", print_r($_POST,true), "</pre";
+    die();
 }
