d9e2d58279344ee61492e09ed06ff4999f2606ee
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 16:50:47 +0500

    changed

--- webroot/Blog/assets/database/connect.php
+++ webroot/Blog/assets/database/connect.php
@@ -28,7 +28,7 @@ $sname = $_POST["Polani"];
 $email = $_POST["mhpolanto@gmail.com"];
 $pass1 = $_POST["pass"];
 
-$sql = "INSERT INTO USERS SET firstName = $fname ,lastName = $sname ,email = $email ,pw = $pass1, ad = '1'";  
+$sql = "INSERT INTO USERS SET firstName = '$fname' ,lastName = '$sname' ,email = '$email' ,pw = '$pass1', ad = '1'";  
 
 
 if ($conn->query($sql) === TRUE) 
