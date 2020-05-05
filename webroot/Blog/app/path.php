60a056bb925ab9df564c68d7a0967802b8a58e7a
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 15:35:54 +0500

    changed

---
+++ webroot/Blog/app/path.php
@@ -0,0 +1,4 @@
+<?php
+define(BASE_PATH, realpath(dirname(__FILE__)));
+var_dump(ROOT_PATH);
+?>
--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -4,8 +4,10 @@ if (isset($_POST['register-btn'])) //if this button is clicked
 {
    unset($_POST['register-btn'], $_POST['passwordConfirmation']);
    $_POST['admin'] = 0;
+   $_POST['password'] = password_hash($_POST['password'],PASSWORD_DEFAULT);
+   
    var_dump($_POST);
-   die();
+   die(); 
 //    echo "<pre>", print_r($value,true),"</pre>";
    
 }
