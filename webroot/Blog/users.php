9d6ec68f974c8d2caa2f7e2642c06628103c750a
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-05 15:07:49 +0500

    changed

--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -1,10 +1,11 @@
 
 <?php
-if (isset($_POST['register-btn']))
+if (isset($_POST['register-btn'])) //if this button is clicked
 {
    unset($_POST['register-btn'], $_POST['passwordConfirmation']);
    $_POST['admin'] = 0;
    var_dump($_POST);
+   die();
 //    echo "<pre>", print_r($value,true),"</pre>";
    
 }
