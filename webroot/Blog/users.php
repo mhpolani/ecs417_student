5dbbe551575e8a343cb3313aa7ee1ad0a2063f47
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 16:26:21 +0500

    changed

--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -1,5 +1,8 @@
 <?php
 include('db.php');
+include('index.php');
+
+
 $errors = array();
 $username = '';
 $email = '';
@@ -45,8 +48,8 @@ if (isset($_POST['register-btn']))
         $_SESSION['admin'] = $user['admin'];
         $_SESSION['message'] = 'You are now logged in!';
         $_SESSION['type'] = 'success';
-        // header('location: index.php');
-        require_once('index.php');
+        header('location: index.php');
+        // require_once('index.php');
         exit();
         
     }   
