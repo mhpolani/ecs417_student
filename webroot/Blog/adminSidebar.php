394cbd6b25ecb8584dc244b19e71d90dfa308d73
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 22:06:36 +0500

    changed

--- webroot/Blog/adminSidebar.php
+++ webroot/Blog/adminSidebar.php
@@ -1,7 +1,7 @@
 <div class="left-sidebar">
                 <ul>
-                    <li><a href="../posts/index.html">Manage Posts</a></li>
-                    <li><a href="../users/index.html">Manage Users</a></li>
+                    <li><a href="../posts/index.php">Manage Posts</a></li>
+                    <li><a href="../users/index.php">Manage Users</a></li>
                     <li><a href="index.php">Manage Topics</a></li>
                 </ul>
             </div>
--- webroot/Blog/users/edit.php
+++ webroot/Blog/users/edit.php
@@ -18,18 +18,20 @@
     <!-- Custom Styling -->
   	<link rel = "stylesheet" href = "../admin.css" type = "text/css">
       
-     <title>Admin Section - Manage Users</title>
+     <title>Admin Section - Edit User</title>
   </head>
   <body>
-  <?php include("../adminHeader.php"); ?>
   	
+      <?php include('../adminHeader.php'); ?>
       
 	  <!-- Admin Page Wrapper -->
 	  <div class = "admin-wrapper">
+	
+      <?php include('../adminSidebar.php'); ?>
+            
+            
 
-      <?php include("../adminSidebar.php"); ?>
-       
-            <!-- Admin Content -->
+            <!-- Admin Content -->            
             <div class="admin-content">
                 <div class="button-group">
                     <a href="create.php" class="btn btn-big">Add User</a>
@@ -38,34 +40,40 @@
             
 
             <div class = "content">
-                <h2 class="page-title">Manage user</h2>
-                <table>
-                    <thead>
-                        <th>#</th>
-                        <th>Username</th>
-                        <th>Role</th>
-                        <th colspan="2">Action</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Hamza</td>
-                            <td>Admin</td>
-                            <td><a href="#" class = "edit">Edit</a></td>
-                            <td><a href="#" class = "delete">Delete</a></td>
-                            
-                        </tr>
-                         <tr>
-                            <td>1</td>
-                            <td>Polani</td>
-                            <td>Editor</td>
-                            <td><a href="#" class = "edit">Edit</a></td>
-                            <td><a href="#" class = "delete">Delete</a></td>
-                            
-                        </tr>
 
-                    </tbody>
-                </table>
+                <h2 class="page-title">Add User</h2>
+
+               <form action="create.html" method = "post">
+                    <div>
+              <label>Username</label>
+              <input type = "text" name = "username" class = "text-input">
+          </div>
+          <div>
+              <label>Email</label>
+              <input type = "email" name = "email" class = "text-input">
+          </div>
+          <div>
+              <label>Password</label>
+              <input type = "password" name = "password" class = "text-input">
+          </div>
+          <div>
+              <label>Confirm Password</label>
+              <input type = "password" name = "passwordConfirmation" class = "text-input">              
+          </div>
+          <div>
+                <label>Role</label>
+              <select name="role" class = "text-input">
+                  <option value="Admin">Admin</option>
+                  <option value="Editor">Editor</option>
+              </select>  
+            </div>
+
+                <div>
+                    <button type = "submit" class = "btn btn-big">Update User</button>
+                </div>
+
+               </form>
+               
             </div>
       </div>
 
@@ -78,7 +86,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>  
 
 	<!-- Custom Script -->
-	<script src = "../../scripts.js"></script>
+	<script src = "../scripts.js"></script>
 
   </body>
   </html>
