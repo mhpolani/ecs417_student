81b0089f34b4b17cda0cb0e46fce87ebf1c73131
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 16:54:41 +0500

    changed

---
+++ webroot/Blog/header.php
@@ -0,0 +1,32 @@
+<header>
+      <a href = "index.php" class = "logo">
+  			<h1 class = "logo-text"><span>Hamza</span>Productions</h1>
+        </a>
+  		<i class = "fa fa-bars menu-toggle"></i>
+  		<ul class = "nav">
+  			<li><a href = "index.php" class = "navbar">Home</a></li>
+  			<li><a href = "#" class = "navbar">About</a></li>
+  			<li><a href = "#" class = "navbar">Services</a></li>
+			
+			<?php if(isset($_SESSION['ID'])): ?>
+			
+			<li>
+				<a href = "#">
+					<i class="fas fa-user-tie"></i>
+					<?php echo $_SESSION['username']; ?>
+					<i class="fas fa-angle-double-down" style = "font-size:.8em"></i>
+				</a>
+				<ul>
+				<li><a href = "#">Dashboard</a></li>
+				<li><a href = "#" class = "logout">Logout</a></li>
+			 </ul>
+  		</li>
+
+			<?php else: ?>
+			<li><a href = "#"> Sign Up </a></li>
+			<li><a href = "#"> Login </a></li>
+			
+			<?php endif; ?>
+
+  		</ul>
+	  </header>
