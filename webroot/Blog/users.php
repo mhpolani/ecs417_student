db858393f8429207b679d96ea631e0d152829148
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 05:48:03 +0500

    changed

--- webroot/Blog/users.php
+++ webroot/Blog/users.php
@@ -2,6 +2,14 @@
 include('db.php');
 if (isset($_POST['register-btn']))
 {
+    $errors = array();
+    
+    if (empty($_POST['username']))   // to prevent a user with null details to be created (validation)
+    {
+        array_push($errors, 'Username is required');
+    }
+    dd($errors);
+
     unset($_POST['register-btn'], $_POST['passwordConfirmation']);
     $_POST['admin'] = 0;
     $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
