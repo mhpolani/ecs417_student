3a7395e6b9926e942d70a425156e4a6e14b28746
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-07 02:59:30 +0500

    changed

--- webroot/Blog/posts.php
+++ webroot/Blog/posts.php
@@ -3,10 +3,13 @@
 include('db.php');
 include('validatePosts.php');
 
+$table = 'POSTS';
+$errors = array();
 if (isset($_POST['add-post']))
 {
     unset($_POST['add-post']);
     $_POST['USER_ID'] = 1;
     $_POST['PUBLISHED'] =1;
+    $post = create($table,$_POST);
     dd($_POST);
 }
