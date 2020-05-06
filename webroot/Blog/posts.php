af4a57f8b4884cda03a566371d6a2cdbba7c3a45
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-07 03:29:39 +0500

    changed

--- webroot/Blog/posts.php
+++ webroot/Blog/posts.php
@@ -1,9 +1,50 @@
 <?php 
-
-include('db.php');
+session_start();
+require('connect.php');
+// include('db.php');
 include('validatePosts.php');
 
+$sql = "INSERT INTO $table SET ";
 $table = 'POSTSDEMO';
+//DEBUGGING
+function create($table,$data)
+{
+    global $conn;
+    // $sql = "INSERT INTO $table SET ";
+    
+    $i = 0;
+    foreach($data as $key => $value)        
+    {
+        if ($i === 0)
+        {
+            $sql = $sql . " $key=?";
+        }
+        else
+        {
+            $sql = $sql . ", $key=?";                
+        }
+        $i++;
+    }
+    
+    $stmt = executeQuery($sql,$data);
+    $id = $stmt->insert_id;   //grab the id of the record
+    return $id;
+}
+
+function executeQuery($sql,$data)
+{
+    global $conn;
+    $stmt = $conn->prepare($sql);
+    $values = array_values($data);
+    $types = str_repeat('s',count($values));
+    $stmt->bind_param($types, ...$values);
+    $stmt->execute();
+    return $stmt;
+}
+
+// END OF DEBUGGING
+
+
 $errors = array();
 if (isset($_POST['add-post']))
 {
