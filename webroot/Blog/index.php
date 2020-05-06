2fbcfcf1bd882bfccebf828e89a1a5408038942f
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 17:16:54 +0500

    changed

--- webroot/Blog/index.php
+++ webroot/Blog/index.php
@@ -27,7 +27,12 @@ include('db.php');
 	<?php if(isset($_SESSION['message'])); ?>
 	  <div class="msg <?php echo $_SESSION['type']; ?>">
 	  <li><?php echo $_SESSION['message'];?></li>
+	  <?php 
+		 unset($_SESSION['message']);     //This will remove the login message once the users seen it
+		 unset($_SESSION['type']); 
+	  ?>
 	  </div>
+<?php endif; ?>
 
 	  <!-- Page Wrapper -->
 	  <div class = "page-wrapper">
