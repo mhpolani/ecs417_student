3474368f0b2a9a97b2f3f6f71747e356a3f33531
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 16:59:49 +0500

    changed

--- webroot/Blog/index.php
+++ webroot/Blog/index.php
@@ -1,4 +1,7 @@
-<?php include("path.php")?>
+<?php 
+include("path.php");
+include('db.php');
+?>
 
 <!DOCTYPE html>
  <html lang = "en">
--- webroot/Blog/login.php
+++ webroot/Blog/login.php
@@ -13,28 +13,7 @@
   	<title>Log in</title>
   </head>
   <body>
-  	<header>
-      <a href = "index.php" class = "logo">
-  			<h1 class = "logo-text"><span>Hamza</span>Productions</h1>
-        </a>
-  		<i class = "fa fa-bars menu-toggle"></i>
-  		<ul class = "nav">
-          <li><a href = "index.php" class = "navbar">Home</a></li>
-  			<li><a href = "#">About</a></li>
-  			<li><a href = "#">Services</a></li>
-			<li>
-				<a href = "#">
-					<i class="fas fa-user-tie"></i>
-					Hamza Polani
-					<i class="fas fa-angle-double-down" style = "font-size:.8em"></i>
-				</a>
-				<ul>
-				<li><a href = "#">Dashboard</a></li>
-				<li><a href = "#" class = "logout">Logout</a></li>
-			 </ul>
-  		</li>
-  		</ul>
-	  </header>
+  <?php include('header.php'); ?>
 
       <div class="author-content">
 
