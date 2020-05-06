1ce58e835957160ea229bfbcaf0aede76205ed8d
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-07 03:20:10 +0500

    changed

--- webroot/Blog/posts.php
+++ webroot/Blog/posts.php
@@ -3,13 +3,13 @@
 include('db.php');
 include('validatePosts.php');
 
-$table = 'POSTS';
+$table = 'POSTSDEMO';
 $errors = array();
 if (isset($_POST['add-post']))
 {
     unset($_POST['add-post']);
     $_POST['USER_ID'] = 1;
-    $_POST['PUBLISHED'] =1;
+    $_POST['PUBLISHED'] = 1;
     $post_id = create($table,$_POST);  //create methods always returns the id of the record it creates
     dd($post_id);
 }
