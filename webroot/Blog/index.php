e1d9bc61f272ae1889a84c0dad96b23a6f2c6aa5
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-06 16:41:27 +0500

    changed

--- webroot/Blog/index.php
+++ webroot/Blog/index.php
@@ -28,6 +28,29 @@
   			<li><a href = "index.php" class = "navbar">Home</a></li>
   			<li><a href = "#" class = "navbar">About</a></li>
   			<li><a href = "#" class = "navbar">Services</a></li>
+			
+			<?php if(isset($_SESSION['ID'])): ?>
+			
+			<li>
+				<a href = "#">
+					<i class="fas fa-user-tie"></i>
+					<?php echo $_SESSION['username']; ?>
+					<i class="fas fa-angle-double-down" style = "font-size:.8em"></i>
+				</a>
+				<ul>
+				<li><a href = "#">Dashboard</a></li>
+				<li><a href = "#" class = "logout">Logout</a></li>
+			 </ul>
+  		</li>
+
+			<?php else: ?>
+			<li><a href = "#"> Sign Up </a></li>
+			<li><a href = "#"> Login </a></li>
+			
+			<?php endif; ?>
+
+			
+			
 			<li>
 				<a href = "#">
 					<i class="fas fa-user-tie"></i>
