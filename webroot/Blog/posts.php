3c54cddafc9635fa0b69556f1202970a4a90d217
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-07 05:35:11 +0500

    changed

--- webroot/Blog/posts.php
+++ webroot/Blog/posts.php
@@ -12,10 +12,10 @@ if (isset($_POST['add-post']))
     unset($_POST['add-post']);
     $_POST['USER_ID'] = 1;
     $_POST['PUBLISHED'] = 1;    
-    dd($post_id);
+    dd($_POST);
     // $post_id = create($table,$_POST);  //create methods always returns the id of the record it creates
     // global $conn;
-    // $sql = "INSERT INTO ha ('USER_ID', 'TITLE', 'IMAGE' 
+    // $sql = "INSERT INTO ha ('USER_ID', 'TITLE', 'IMAGE', 'BODY', 'PUBLISHED') VALUES 
     // $stmt = $conn->prepare($sql);
     // $values = array_values($data); //  The array_values() function returns an array containing all the values of an array
     // $types = str_repeat('s',count($values));
