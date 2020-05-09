ad23133b7254729fb15a627a4ff64fbfa15d368b
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-10 02:07:38 +0500

    changed

--- webroot/Blog2/posts.php
+++ webroot/Blog2/posts.php
@@ -72,19 +72,20 @@ if (isset($_POST['add-post']))   //vvvv delicate
     if(count($errors) == 0)
     {
     unset($_POST['add-post']);
-    $_POST['user_id'] = $_SESSION['ID'];
-    $_POST['published'] = isset($_POST['published']) ? 1 : 0;    //Since the 'published' is of type tinyint(boolean) if the published button is clicked, value is set to 1
-    $_POST['body'] = htmlentities($_POST['body']);
-    $post_id = create($table,$_POST);
-    $_SESSION['message'] = "Post created successfully.";
-    $_SESSION['type'] = "success";
-    header('location: postsindex.php');
-    }
-    else
-    {
-        $title = $_POST['title'];
-        $body = $_POST['body'];
-    }
+    dd($_POST);
+    // $_POST['user_id'] = $_SESSION['ID'];
+    // $_POST['published'] = isset($_POST['published']) ? 1 : 0;    //Since the 'published' is of type tinyint(boolean) if the published button is clicked, value is set to 1
+    // $_POST['body'] = htmlentities($_POST['body']);
+    // $post_id = create($table,$_POST);
+    // $_SESSION['message'] = "Post created successfully.";
+    // $_SESSION['type'] = "success";
+    // header('location: postsindex.php');
+    // }
+    // else
+    // {
+    //     $title = $_POST['title'];
+    //     $body = $_POST['body'];
+    // }
 }
 
 if (isset($_POST['edit-post']))  //update code
