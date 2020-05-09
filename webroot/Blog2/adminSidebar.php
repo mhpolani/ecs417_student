587982e1bd4a004dd15d463135c8fc6c4055015b
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-09 17:35:13 +0500

    changed

--- webroot/Blog2/adminSidebar.php
+++ webroot/Blog2/adminSidebar.php
@@ -1,6 +1,6 @@
 <div class="left-sidebar">
                 <ul>
-                    <li><a href="../postsindex.php">Manage Posts</a></li>
+                    <li><a href="postsindex.php">Manage Posts</a></li>
                     <!-- <li><a href="../users/index.php">Manage Users</a></li> -->
                     <!-- <li><a href="../topics/index.php">Manage Topics</a></li> -->
                 </ul>
