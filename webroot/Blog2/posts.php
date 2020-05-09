91ad9d207f87945c86f65f2a5fcf360e048bf64f
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-10 02:17:10 +0500

    changed

--- webroot/Blog2/posts.php
+++ webroot/Blog2/posts.php
@@ -72,7 +72,7 @@ if (isset($_POST['add-post']))   //vvvv delicate
     if(count($errors) == 0)
     {
     unset($_POST['add-post']);
-    dd($_POST);
+    
     $_POST['user_id'] = $_SESSION['ID'];
     $_POST['published'] = isset($_POST['published']) ? 1 : 0;    //Since the 'published' is of type tinyint(boolean) if the published button is clicked, value is set to 1
     $_POST['body'] = htmlentities($_POST['body']);
