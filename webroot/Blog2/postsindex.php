f2902eebae36496a5eda0eb3eb80a7da738467e4
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-11 02:40:59 +0500

    changed

--- webroot/Blog2/postsindex.php
+++ webroot/Blog2/postsindex.php
@@ -58,7 +58,7 @@
                         <tr>
                             <td><?php echo $key + 1; ?></td>
                             <td><?php echo $post['title'] ?></td>
-                            <td>Hamza</td>
+                            <td>Ali</td>
                             <td><a href="edit.php?published=1&p_id=<?php echo $post['ID']?>" class = "preview">Preview</a></td>                        
                             <!-- <td><a href="edit.php?ID=<?php echo $post['ID']; ?> " class = "edit">Edit</a></td> -->
                             <td><a href="edit.php?delete_id=<?php echo $post['ID']; ?> " class = "edit">Delete</a></td>
