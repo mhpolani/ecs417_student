b40603e470ce0dca9f54666bdb344217c2f753d1
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-12 04:54:38 +0500

    changed

--- webroot/NANA/messages.php
+++
@@ -1,9 +0,0 @@
-<?php if(isset($_SESSION['message'])): ?>
-	  <div class="msg <?php echo $_SESSION['type']; ?>">
-	  <li><?php echo $_SESSION['message'];?></li>
-	  <?php 
-		 unset($_SESSION['message']);     //This will remove the login message once the users seen it
-		 unset($_SESSION['type']); 
-	  ?>
-	  </div>
-<?php endif; ?>
