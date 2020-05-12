f2467797df00dd3d360635c739a5b9f4e50367e7
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-12 15:56:37 +0500

    changed

--- webroot/Blog/index.php
+++ webroot/Blog/index.php
@@ -43,8 +43,7 @@ $posts = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 		  <!-- Post Slider -->
 		<div class = "post-slider">
 
-			<!-- <h1 class = "slider-title">What's Trending?</h1> -->
-			<h1 class = "slider-title">Fuck off?</h1>
+			<h1 class = "slider-title">What's Trending?</h1>			
 			<i class = "fas fa-chevron-left prev"></i>
 			<i class = "fas fa-chevron-right next"></i>
 			
