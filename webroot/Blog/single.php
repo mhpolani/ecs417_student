f96809c832a2c8fd8a4311e6d41e8db0ccab14b9
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-12 15:59:54 +0500

    changed

--- webroot/Blog/single.php
+++ webroot/Blog/single.php
@@ -7,7 +7,27 @@ if (isset($_GET['ID']))
 
 // $posts = selectAll('POSTS', ['published' => 1]);
 
-include('comment.php');
+// include('comment.php');
+
+if (isset($_POST['add-comment']))  
+{    
+       
+    dd($_POST);
+    // unset($_POST['add-post']);
+    // $_POST['user_id'] = $_SESSION['ID'];
+    // $_POST['published'] = isset($_POST['published']) ? 1 : 0;    //Since the 'published' is of type tinyint(boolean) if the published button is clicked, value is set to 1
+    // $_POST['body'] = htmlentities($_POST['body']);
+    // $post_id = create($table,$_POST);
+    // $_SESSION['message'] = "Post created successfully.";
+    // $_SESSION['type'] = "success";
+    // header('location: postsindex.php');
+    
+    // else
+    // {
+    //     $title = $_POST['title'];
+    //     $body = $_POST['body'];
+    // }
+}
 
 ?>
 <!DOCTYPE html>
