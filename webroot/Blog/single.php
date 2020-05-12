37cc2378930f55e83611df4e7f63341d8ecad4ae
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-12 16:12:40 +0500

    changed

--- webroot/Blog/single.php
+++ webroot/Blog/single.php
@@ -13,8 +13,8 @@ if (isset($_POST['add-comment']))
 {    
        
     dd($_POST);
-    // unset($_POST['add-post']);
-    // $_POST['user_id'] = $_SESSION['ID'];
+    unset($_POST['add-comment']); 
+    $_POST['user_id'] = $_SESSION['ID'];
     // $_POST['published'] = isset($_POST['published']) ? 1 : 0;    //Since the 'published' is of type tinyint(boolean) if the published button is clicked, value is set to 1
     // $_POST['body'] = htmlentities($_POST['body']);
     // $post_id = create($table,$_POST);
