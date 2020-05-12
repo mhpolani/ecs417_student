76c1403dd743526b9081f2f7288c3e873e98dab1
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-12 22:29:46 +0500

    changed

--- webroot/Blog/single.php
+++ webroot/Blog/single.php
@@ -4,15 +4,16 @@ if (isset($_GET['ID']))
 {
 	$post = selectOne('POSTS',['ID' => $_GET['ID']]); //fetch post clicked from index and display fetched post on single.php
 }
-$_POST['postid'] = $post['ID'];
+
 if (isset($_POST['add-comment']))  
 {    
-    unset($_POST['add-comment']); 
+	unset($_POST['add-comment']); 
+	$_POST['postid'] = $post['ID'];
 	$_POST['userid'] = $_SESSION['ID'];
 	$id = create('COMMENTS', $_POST);
 	$_SESSION['message'] = "Comment added successfully.";
 	$_SESSION['type'] = "success";	
-	header("index.php"); 
+	// header("index.php"); 
 }
 // 	$noOfComments++;
 	
