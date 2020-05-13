f695f9c47786ea51b8281e746c844af024b8eedd
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-13 17:13:01 +0500

    changed

--- webroot/Blog/index.php
+++ webroot/Blog/index.php
@@ -124,7 +124,7 @@ $posts = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 					  <ul>
 						
 					  <?php foreach($months as $key => $month): ?>
-						<li><a href = "<?php echo 'index.php?id=' . $month['id'] . '&name=' . $month['name']?>"><?php echo $month['name']; ?></a></li>
+						<li><a href = "<?php echo 'index.php?month_id=' . $month['id'] . '&name=' . $month['name']?>"><?php echo $month['name']; ?></a></li>
 					  <?php endforeach; ?>														
 					  <!-- <li><a href = "#">About Me</a></li>
 						  <li><a href = "#">Skills and Achievements</a></li>
