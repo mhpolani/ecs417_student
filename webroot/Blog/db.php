7ef3f07949d4040d77f07c6efb2f45b6e9b59421
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-13 05:56:27 +0500

    changed

--- webroot/Blog/db.php
+++ webroot/Blog/db.php
@@ -171,5 +171,5 @@ function searchPosts($term)  //functionality for searchbar and ultimately months
     $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     return $records;   
 }
--- //The selectAll function returns all the records in the database, provided that the conditions, if passed, are met.
--- //All the records in the table represent an array that in turn holds arrays, each of which represents a record.
+ //The selectAll function returns all the records in the database, provided that the conditions, if passed, are met.
+ //All the records in the table represent an array that in turn holds arrays, each of which represents a record.
