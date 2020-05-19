e09ff96022535c6cea2ef67fc0fafa2b36e94838
Macbook Air <macbookair@MHPolantos-Macbook-Air.local>
2020-05-19 07:49:21 +0500

    changed

--- webroot/Blog/viewBlog.php
+++ webroot/Blog/viewBlog.php
@@ -201,6 +201,9 @@ $posts = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 		  </footer>
 	  <!-- End of footer -->
 
+	<!-- JQuery -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>  
+
 	<!-- SLICK Carousel -->
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
